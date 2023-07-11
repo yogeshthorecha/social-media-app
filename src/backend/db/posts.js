82e27c7aca9d4d3a5541e7dbc52a16da71246828
yogeshthorecha <118168243+yogeshthorecha@users.noreply.github.com>
2023-07-11 15:16:13 +0530

    Update posts.js

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -90,7 +90,7 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      ".",
+      "My mad friend",
     likes: {
       likeCount: 0,
       likedBy: [],
@@ -106,17 +106,114 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "Vibes.",
+      "cutie or wotttt.",
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
     website: "https://www.wikipedia.com",
-    image: "https://i.postimg.cc/dVqPGxqs/image.png",
-    username: "K_Sheru",
+    image: "https://i.postimg.cc/L41qh5Hh/image.png",
+    username: "P_dogelina",
+    createdAt: "2021-06-14T12:00:00Z",
+    updatedAt: formatDate(),
+    trending:false
+  },
+  {
+    _id: uuid(),
+    content:
+      "stay classy.",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    website: "https://www.wikipedia.com",
+    image: "https://i.postimg.cc/W4v8YCXw/image.png",
+    username: "Chimtu10",
+    createdAt: "2021-06-14T12:00:00Z",
+    updatedAt: formatDate(),
+    trending:false
+  },
+  {
+    _id: uuid(),
+    content:
+      "colorful day it is....",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    website: "https://www.wikipedia.com",
+    image: "https://i.postimg.cc/wTt1ykDX/image.png",
+    username: "S_bruno",
+    createdAt: "2021-06-14T12:00:00Z",
+    updatedAt: formatDate(),
+    trending:false
+  },
+  {
+    _id: uuid(),
+    content:
+      "Don't mess with me....",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    website: "https://www.wikipedia.com",
+    image: "https://i.postimg.cc/rmxKZyFf/image.png",
+    username: "D_Doggy",
+    createdAt: "2021-06-14T12:00:00Z",
+    updatedAt: formatDate(),
+    trending:false
+  },
+  {
+    _id: uuid(),
+    content:
+      "Full Emditing...hue hue hue",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    website: "https://www.wikipedia.com",
+    image: "https://i.postimg.cc/ZnW6qzT3/image.png",
+    username: "D_Doggy",
+    createdAt: "2021-06-14T12:00:00Z",
+    updatedAt: formatDate(),
+    trending:true
+  },
+  {
+    _id: uuid(),
+    content:
+      "side please, cutie is coming",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    website: "https://www.wikipedia.com",
+    image: "https://i.postimg.cc/7Y0rCCts/image.png",
+    username: "O_Daisy",
+    createdAt: "2021-06-14T12:00:00Z",
+    updatedAt: formatDate(),
+    trending:true
+  },
+  {
+    _id: uuid(),
+    content:
+      "Oh common......I am on dieting",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    website: "https://www.wikipedia.com",
+    image: "https://i.postimg.cc/L54gqK2v/image.png",
+    username: "B_Shiro",
     createdAt: "2021-06-14T12:00:00Z",
     updatedAt: formatDate(),
     trending:false
   },
 ];
+
